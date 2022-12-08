22f648bae27b8a36a740fb500f033efc5473be3b
StungDev <103583641+StungDev@users.noreply.github.com>
2022-12-08 11:49:08 +0000

    keys without the database

--- src/api.js
+++ src/api.js
@@ -1,3 +1,4 @@
+//api to send and create keys
 const express = require('express');
 const serverless = require('serverless-http');
 
@@ -9,38 +10,45 @@ const jsonParser = bodyParser.json();
 const getRequests = {};
 const postRequests = {};
 
-async function getKey(req) {
-  const { id } = req.params;
-  return id;
+let keys = [];
+//get user key
+getRequests.getkey = async function getApiKey(req) {
+  const { user } = req.query;
+  const key = keys.find((key) => key.user === user);
+  const success = key ? true : false;
+  return { userinfo: key, success: success };
 }
-
-async function callApi(req, res, requests, query) {
-  const { id } = req.params;
-  const request = requests[id];
-  if (request) {
-    res.json(request);
-  } else {
-    res.status(404).send('Not found');
+//create user key
+postRequests.createkey = async function createApiKey(req) {
+  const { user } = req.body;
+  const key = keys.find((key) => key.user === user);
+  if (key) {
+    return { success: false, message: 'User already exists' };
   }
+  const newKey = {
+    user: user,
+    key: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+  };
+  keys.push(newKey);
+  return { success: true, userinfo: newKey };
 }
-  
 //handles all api calls
 
 function handleApiCall(req, res, requests) {
-  const { id } = req.params;
-  const request = requests[id];
+  const apiname = req.params[0];
+  const request = requests[apiname];
   if (request) {
-    res.json(request);
+    res.json(request(req));
   } else {
     res.status(404).send('Not found');
   }
 }
 
-router.get('/get', jsonParser, (req, res) => {
-  // callApi(req, res, getRequests, req.query);
-  // console.log(req)
-
-  res.json({req: req})
+router.get('/get', (req, res) => {
+  handleApiCall(req, res, getRequests);
+});
+router.post('/post', jsonParser, (req, res) => {
+  handleApiCall(req, res, postRequests);
 });
 
 app.use('/.netlify/functions/api', router);
